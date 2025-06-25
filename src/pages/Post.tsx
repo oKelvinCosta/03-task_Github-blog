9757bf04359ef7809a58277959a8d819db2b6c45
Kelvin Costa <okelvincosta@gmail.com>
2025-06-25 17:23:03 -0300

    🎉 Commit init

---
+++ src/pages/Post.tsx
@@ -0,0 +1,80 @@
+import Img from "../Img";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
+import { CardAction } from "../components/ui/card";
+import { CalendarClock, ChevronLeft, Github, MessageSquareMore, SquareArrowOutUpRight } from "lucide-react";
+
+export default function Home() {
+  return (
+    <>
+      <header>
+        <div className="flex items-center justify-center">
+          <Img src={`./imgs/core/placeholder.png`} className={``} />
+        </div>
+        <Card className="flex items-center">
+          <Img src={`./imgs/core/placeholder.png`} className={``} />
+          <div>
+            <div>
+              <a href="">
+                <ChevronLeft size={16} color="#3294f8" />
+                VOLTAR
+              </a>
+              <CardAction>
+                VER NO GITHUB <SquareArrowOutUpRight size={16} color="#3294f8" />
+              </CardAction>
+            </div>
+            <CardHeader className="flex items-center justify-between">
+              <CardTitle>JavaScript data types and data structures</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean
+                malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+              </p>
+              <div className="flex items-center gap-6">
+                <a href="https://github.com/cameronwll">
+                  <Github size={16} color="#3a526b" />
+                  cameronwll
+                </a>
+                <a href="https://github.com/cameronwll">
+                  <CalendarClock size={16} color="#3a526b" />
+                  Rocketseat
+                </a>
+                <a href="https://github.com/cameronwll">
+                  <MessageSquareMore size={16} color="#3a526b" />
+                  32 seguidores
+                </a>
+              </div>
+            </CardContent>
+          </div>
+        </Card>
+      </header>
+
+      <div className="flex gap-6">
+        <div className="flex items-center justify-between">
+          <h3>Publicações</h3>
+          <span>6 publicações</span>
+        </div>
+        <div>FORM</div>
+      </div>
+
+      <div className="flex gap-6">
+        <Card className="flex items-center">
+          <Img src={`./imgs/core/placeholder.png`} className={``} />
+          <div>
+            <CardHeader className="flex items-center justify-between">
+              <CardTitle>JavaScript data types and data structures</CardTitle>
+              <span>Há 1 dia</span>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Programming languages all have built-in data structures, but these often differ from one language to
+                another. This article attempts to list the built-in data structures available in JavaScript and what
+                properties they have.
+              </p>
+            </CardContent>
+          </div>
+        </Card>
+      </div>
+    </>
+  );
+}
